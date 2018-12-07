8a163de19bc65944048be68c99fbf54ea9dabe84
garyho1998 <44996214+garyho1998@users.noreply.github.com>
2018-12-07 10:37:50 +0800

    Update NameToID.py

--- NameToID.py
+++ NameToID.py
@@ -7,6 +7,7 @@ from numpy import NaN
 from pandas.tests.frame.test_validate import dataframe
 from unittest.mock import inplace
 from matplotlib.testing.compare import converter
+from path_definition import ROOT_DIR
 
 start = time.time()
 def f5(seq, idfun=None): 
@@ -20,8 +21,9 @@ def f5(seq, idfun=None):
        if marker in seen: continue
        seen[marker] = 1
        result.append(item)
-   return result
-def convert_nameToID(cell):                  
+   return result
+
+def convert_nameToID_pitcher(cell):                  
     if cell ==" " or cell =="" or cell=="NaN" or cell==NaN:
         return NaN
     else:
@@ -31,10 +33,22 @@ def convert_nameToID(cell):
             else:       
                 return dict_df.loc[cell]['playerID'].iloc[-1]
         else: 
-            NotFoundList.append(cell)
+            NotFoundList_pitcher.append(cell)
+            return NaN
+def convert_nameToID_batter(cell):                  
+    if cell ==" " or cell =="" or cell=="NaN" or cell==NaN:
+        return NaN
+    else:
+        if cell in dict_df.index:
+            if type(dict_df.loc[cell]['playerID']) is str:
+                return dict_df.loc[cell]['playerID']
+            else:       
+                return dict_df.loc[cell]['playerID'].iloc[-1]
+        else: 
+            NotFoundList_batter.append(cell)
             return NaN
         
-folderpath = "//OAFILE06/gcyho$/Desktop/AIData/Master.xlsx"
+folderpath = ROOT_DIR + "data/Master.xlsx"
 master_df = pd.read_excel(folderpath,na_values=[""])
 
 dict_df = master_df[['playerID','nameFirst','nameLast','nameGiven']]
@@ -43,46 +57,38 @@ dict_df['name'] = dict_df[['nameFirst', 'nameLast']].apply(lambda x: ' '.join(x.
 dict_df['name'] = dict_df['name'].apply(lambda x: str(x))
 dict_df.set_index('name', inplace=True)
 
-folderpath = "//OAFILE06/gcyho$/Desktop/AIData/2016game"
+folderpath = ROOT_DIR + "games/2016game"
 
 temp_dfs = []
-NotFoundList = []
+NotFoundList_pitcher = []
+NotFoundList_batter = []
 for i in range(3,29):
     temp_dfs.append( pd.read_excel(folderpath + str(i) + ".xlsx") )
 game_df = pd.concat(temp_dfs)
 game_df = game_df.reset_index(drop=True)
 print (game_df)
 
-converter_dict = {'pitcher1':convert_nameToID, 'pitcher2':convert_nameToID}
+converter_dict = {'pitcher1':convert_nameToID_pitcher, 'pitcher2':convert_nameToID_pitcher}
 for i in range(1,13):
-    converter_dict['batter'+ str(i)]=convert_nameToID
+    converter_dict['batter'+ str(i)]=convert_nameToID_batter
 game_df.iloc[:,3:17] = game_df.apply(converter_dict)
-print (game_df)
-print((len(game_df) - game_df.count())/len(game_df))
 
-NotFoundList = f5(NotFoundList)
-NotFoundList.sort(key=None, reverse=False)
-f = open("//OAFILE06/gcyho$/Desktop/AIData/NameToIDLog.txt", "w")
-print(len(NotFoundList))
-f.write(str(NotFoundList))
+game_df['num_of_batter'] = game_df.iloc[:,3:15].apply(lambda x: x.count(), axis=1)
+game_df['num_of_pitcher'] = game_df.iloc[:,15:17].apply(lambda x: x.count(), axis=1)
 
-writer = pd.ExcelWriter("//OAFILE06/gcyho$/Desktop/AIData/level2/2016game.xlsx")
+NotFoundList_pitcher = f5(NotFoundList_pitcher)
+NotFoundList_pitcher.sort(key=None, reverse=False)
+f = open( ROOT_DIR + "NotFoundList_pitcher.txt", "w")
+f.write(str(NotFoundList_pitcher))
+
+NotFoundList_batter = f5(NotFoundList_batter)
+NotFoundList_pitcher.sort(key=None, reverse=False)
+f2 = open(ROOT_DIR + "NotFoundList_batter.txt", "w")
+f2.write(str(NotFoundList_batter))
+
+writer = pd.ExcelWriter(ROOT_DIR + "level2/2016game2.xlsx")
 game_df.to_excel(writer,'Sheet1')
 writer.save()
 
 end = time.time()
 print((end-start)/60)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
